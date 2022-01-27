e6b38ecadeedba84accd33d91e2725c26a0cf506
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-01-27 15:59:00 +0300

    :white_check_mark: More `step 2` tests

--- test/functions/makeEvenTree.spec.ts
+++ test/functions/makeEvenTree.spec.ts
@@ -23,6 +23,13 @@ class CompressedNode<T extends character> {
         }
         return root;
     }
+
+    public static getArray<T extends character>(length: number) {
+        if (length < 0) {
+            throw new RangeError("array length must be greater than 0");
+        }
+        return new Array(length).fill(0).map((_) => new CompressedNode<T>());
+    }
 }
 
 function areArraysEqual<T>(first: T[], second: T[]): boolean {
@@ -73,15 +80,18 @@ test("Two edges chain. o-o-o", () => {
     expectSuffixTreesEquality(even, desired.decompress()).toBe(true);
 });
 
-test("Tho edges fork", () => {
-    const tree = new CompressedNode<number>([[0], [1]], [new CompressedNode<number>(), new CompressedNode<number>()]);
+//     O            O
+//  0/  \1  --> ab/  \cd
+//  O   O        O   O
+test("Two edges fork", () => {
+    const tree = new CompressedNode<number>([[0], [1]], CompressedNode.getArray(2));
     const pairs: Pair<string>[] = [new Pair("a", "b"), new Pair("c", "d")];
     const desired = new CompressedNode<string>(
         [
             ["a", "b"],
             ["c", "d"],
         ],
-        [new CompressedNode<string>(), new CompressedNode<string>()]
+        CompressedNode.getArray(2)
     );
 
     const even = makeEvenTree(tree.decompress(), pairs);
@@ -98,13 +108,95 @@ test("Two edges fork with same first character", () => {
     const pairs: Pair<string>[] = [new Pair("a", "b"), new Pair("a", "c")];
     const desired = new CompressedNode<string>(
         [["a"]],
-        [new CompressedNode([["b"], ["c"]], [new CompressedNode(), new CompressedNode()])]
+        [new CompressedNode([["b"], ["c"]], CompressedNode.getArray(2))]
     );
 
-	const even = makeEvenTree(tree.decompress(), pairs);
+    const even = makeEvenTree(tree.decompress(), pairs);
 
-    console.log(even.toString());
-    console.log(desired.decompress().toString());
-    
     expectSuffixTreesEquality(even, desired.decompress()).toBe(true);
+});
+
+//      O                O              O
+//   0/ |1 \2 -->   ab/ |ac \bc -->   a/ \bc
+//   O  O  O         O  O    O        O  O
+//                                  b/ \c
+//                                  O   O
+test("Three edges fork with both same and different first characters", () => {
+    const tree = new CompressedNode<number>([[0], [1], [2]], CompressedNode.getArray(3));
+    const pairs: Pair<string>[] = [new Pair("a", "b"), new Pair("a", "c"), new Pair("b", "c")];
+
+    const desired = new CompressedNode<string>(
+        [["a"], ["b", "c"]],
+        [new CompressedNode([["b"], ["c"]], CompressedNode.getArray(2)), new CompressedNode()]
+    );
+
+    const even = makeEvenTree(tree.decompress(), pairs);
+
+    expectSuffixTreesEquality(even, desired.decompress()).toBe(true);
+});
+
+//    O           O                O
+//  0/ \1      ab/ \ac             |a
+//  O  O    --> O   O     -->      O
+// 2| 2| \3   za| za| \zz        b/ \c
+//  O  O O     O    O  O         O  O
+//                             za|  |z
+//                               O  O
+//                                a/ \z
+//                                O  O
+test("Double merge recursion test", () => {
+    const tree = new CompressedNode<number>(
+        [[0], [1]],
+        [new CompressedNode([[2]], [new CompressedNode()]), new CompressedNode([[2], [3]], CompressedNode.getArray(2))]
+    );
+
+    const pairs: Pair<string>[] = [new Pair("a", "b"), new Pair("a", "c"), new Pair("z", "a"), new Pair("z", "z")];
+
+    const desired = new CompressedNode<string>(
+        [["a"]],
+        [
+            new CompressedNode(
+                [["b"], ["c"]],
+                [
+                    new CompressedNode([["z", "a"]], [new CompressedNode()]),
+                    new CompressedNode([["z"]], [new CompressedNode([["a"], ["z"]], CompressedNode.getArray(2))]),
+                ]
+            ),
+        ]
+    );
+
+    const even = makeEvenTree(tree.decompress(), pairs);
+    expectSuffixTreesEquality(even, desired.decompress()).toBe(true);
+});
+
+//    O             O           O
+// 02/ \12 --> 4678/ \4578 --> 4|
+//  O  O          O  O          O
+//                          678/ \578
+//                            O  O
+test("Multiple letters in substring with merge", () => {
+    const tree = new CompressedNode<number>(
+        [
+            [0, 2],
+            [1, 2],
+        ],
+        CompressedNode.getArray(2)
+    );
+
+    const pairs: Pair<number>[] = [new Pair(4, 6), new Pair(4, 5), new Pair(7, 8)];
+    const desired = new CompressedNode<number>(
+        [[4]],
+        [
+            new CompressedNode(
+                [
+                    [6, 7, 8],
+                    [5, 7, 8],
+                ],
+                CompressedNode.getArray(2)
+            ),
+        ]
+    );
+
+    const even = makeEvenTree(tree.decompress(), pairs);
+    expectSuffixTreesEquality(even, desired.decompress());
 });
