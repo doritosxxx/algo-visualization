de9f153e25023e07fcbe2b7681f8141301787ade
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-01-27 14:54:55 +0300

    :sparkles: Added SuffixTree `toString` method

--- src/class/SuffixTreeNode.ts
+++ src/class/SuffixTreeNode.ts
@@ -5,5 +5,20 @@ import SuffixTreeEdge from "./SuffixTreeEdge";
 // Узлы содержат массив ребер.
 // Ребра упорядочены по первому символу.
 export default class SuffixTreeNode<T extends character> {
-	public readonly edges : SuffixTreeEdge<T>[] = []
+    public readonly edges: SuffixTreeEdge<T>[] = [];
+
+	// Функция для облегчения отладки.
+    public toString(): string {
+		return this.getStringView(0);
+	}
+
+	private getStringView(depth : number) : string {
+		let result = "";
+
+		for(const edge of this.edges){
+			result += "\t".repeat(depth) + edge.substring + '\n' + edge.endNode.getStringView(depth+1);
+		}
+
+		return result;
+	}
 }
