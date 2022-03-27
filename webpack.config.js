5f28b5db0d8e39ff2028c737620ce026661c418a
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-03-27 14:28:49 +0400

    Enhanced development experience

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
-var path = require('path');
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index',
+    entry: "./src/index",
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+        path: path.resolve(__dirname, "build"),
+        filename: "app.bundle.js",
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+        }),
+    ],
     resolve: {
-        extensions: ['.ts', '.js', '.json']
+        extensions: [".ts", ".js", ".json"],
     },
     module: {
-        rules: [{
-            test: /\.(ts|js)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
+        rules: [
+            {
+                test: /\.(ts|js)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
     },
-	mode: "development"
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build"),
+        },
+        compress: true,
+        port: 9000,
+    },
+	devtool: 'inline-source-map',
 };
