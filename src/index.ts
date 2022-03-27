2e511674b3df16a8740a03d06af619e159c286c2
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-03-27 14:28:59 +0400

    tree

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,74 @@
 import { farach } from "./algorithm/functions";
+import { Edge, Root } from "./algorithm/class";
+import * as d3 from "d3";
+
+type T = Edge<number>;
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("first");
+    const tree = getTree();
 
-	console.log(farach(["1"]))
+    const width = 1000;
+    const height = 600;
+
+    const layout = d3.tree<Edge<number>>().size([width, height])(d3.hierarchy(tree, (node) => node.edges));
+
+    const svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+
+    const edges = svg
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .selectAll("path")
+        .data(layout.links())
+        .enter()
+        .append("path")
+        .attr("d", (d) =>
+            d3.linkVertical()({
+                source: [d.source.x, d.source.y],
+                target: [d.target.x, d.target.y],
+            })
+        );
+
+    const nodes = svg
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .selectAll("circle")
+        .data(layout.descendants())
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => d.x)
+        .attr("cy", (d) => d.y)
+        .attr("r", 10)
+        .style("fill", "green");
+
+    const labels = svg
+        .append("g")
+        .attr("text-anchor", "middle")
+        .selectAll("g")
+        .data(layout.links())
+        .enter()
+        .append("g");
+
+    labels
+        .append("text")
+        .attr("x", (d) => d.source.x / 2 + d.target.x / 2)
+        .attr("y", (d) => d.source.y / 2 + d.target.y / 2)
+        .text((d) => d.source.data.letters[d.source.children.findIndex((e) => e == d.target)]);
 });
+
+function getTree(): Root<number> {
+    const tree = new Root<number>();
+
+    const nested = new Edge([6, 7]);
+    nested.edges = [new Edge([]), new Edge([])];
+
+    tree.edges.push(...[nested, new Edge([]), new Edge([])]);
+    tree.letters.push(...[1, 4, 6]);
+
+	throw new Error("asd")
+
+    return tree;
+}
+
+function update(root: Edge<number>) {}
