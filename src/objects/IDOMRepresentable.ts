48dd8f55c232425cb381981c741065210b317492
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-04-16 15:36:46 +0300

    Stale code

--- src/objects/IDOMRepresentable.ts
+++ src/objects/IDOMRepresentable.ts
@@ -1,4 +1,5 @@
 export default interface IDOMRepresentable {
     unmount();
+	update();
 	domElement : HTMLElement;
 }
--- src/objects/IVisualizable.ts
+++
@@ -1,3 +0,0 @@
-export default interface IVisualizable {
-    update: () => void;
-}
