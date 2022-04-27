d906c04509dd000038b8fff22a5234ba6d6fbafe
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-04-27 17:01:15 +0300

    Edited RootTransition error message

--- src/transitions/RootTransition.ts
+++ src/transitions/RootTransition.ts
@@ -2,9 +2,9 @@ import TransitionBase from "./TransitionBase";
 
 export default class RootTransition extends TransitionBase {
     _introduce() {
-        throw new Error("Method not implemented.");
+        throw new Error("Method shouldn't be invoked.");
     }
     _revoke() {
-        throw new Error("Method not implemented.");
+        throw new Error("Method shouldn't be invoked.");
     }
 }
