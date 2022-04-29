47be568f06ffb1a67eac5e78744c5abd4515dadb
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-04-29 16:25:32 +0300

    Used new PairArray functionality in transition

--- src/transitions/RemoveRepeatingPairsTransition.ts
+++ src/transitions/RemoveRepeatingPairsTransition.ts
@@ -1,35 +1,31 @@
 import { Pair } from "../algorithm/class";
 import { character } from "../algorithm/types";
-import { flatPairArray } from "../helpers";
 import PairArrayView from "../objects/PairArrayView";
 import { SortPairsByFirstElementTransition, TransitionBase } from ".";
+import HighlightRepeatingPairsTransition from "./HighlightRepeatingPairsTransition";
 
 export default class RemoveRepeatingPairsTransition extends TransitionBase {
     public arrayView: PairArrayView;
     public readonly pairs: Pair<character>[];
+    public indices: number[];
 
     _introduce() {
-        const prev = this.previous as SortPairsByFirstElementTransition;
+        const prev = this.previous as HighlightRepeatingPairsTransition;
         this.arrayView = prev.pairArrayView;
+        this.indices = prev.indices;
 
-        this.arrayView.selection.node().classList.remove("highlight-odd");
+        this.arrayView.hideFirstElement();
         this.updateView();
     }
 
     updateView() {
-        const array = flatPairArray(this.pairs);
-        this.arrayView.data(array).join(
-            (enter) => enter,
-            (update) => update.text((d) => d),
-            (exit) => exit.remove()
-        );
+        this.arrayView.remove(this.indices);
     }
 
-    // TODO
     _revoke() {
-        const prev = this.previous as SortPairsByFirstElementTransition;
-        this.arrayView.selection.node().classList.add("highlight-odd");
-        prev.updateView();
+        const prev = this.previous as HighlightRepeatingPairsTransition;
+        this.arrayView.setPairs(prev.pairs);
+        this.arrayView.highlightPairsWithBorder(prev.indices);
 
         this.arrayView = null;
     }
