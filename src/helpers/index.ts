65fdbcfdf61545931ca4832a0cd5b41842445d50
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-01 11:20:55 +0300

    Delete helpers

--- src/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-import { Pair } from "../algorithm/class";
-import { character } from "../algorithm/types";
-
-export function flatPairArray(pairs: Pair<character>[]): string[] {
-    return pairs.flatMap((p) => [p.first, p.second]).map((e) => (e == null ? "$" : e + ""));
-}
