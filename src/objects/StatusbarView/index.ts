022a38bde9263d906748ae1ef0fbd72f7dc6f243
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-05-04 17:16:14 +0300

    Implemeneted status bar with step description

---
+++ src/objects/StatusbarView/index.ts
@@ -0,0 +1,11 @@
+export default class StatusbarView {
+    public container: HTMLDivElement;
+
+    public constructor() {
+        this.container = document.querySelector("#statusbar");
+    }
+
+    public setString(string: string) {
+        this.container.textContent = string;
+    }
+}
--- src/state.ts
+++ src/state.ts
@@ -1,24 +1,33 @@
 import PairArrayView from "./objects/PairArrayView";
 import StackView from "./objects/StackView";
+import StatusbarView from "./objects/StatusbarView";
 import TreeView from "./objects/TreeView";
 
 interface IState {
+    stack: StackView;
+    statusbar: StatusbarView;
     pairArrayView: PairArrayView;
     pairArrayViewClone: PairArrayView;
-    stack: StackView;
-    treeView1: TreeView;
+    evenTreeView: TreeView;
+    oddTreeView: TreeView;
+    mergedTreeView: TreeView;
 }
 
 let state: IState = null;
 const stack = new StackView();
+const statusbar = new StatusbarView();
 
 export function reset() {
     state = {
+        stack: stack,
+        statusbar: statusbar,
+
         pairArrayView: null,
         pairArrayViewClone: null,
-        stack: stack,
 
-        treeView1: null,
+        evenTreeView: null,
+        oddTreeView: null,
+        mergedTreeView: null,
     };
     state.stack.clear();
 }
